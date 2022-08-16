4588e34808bce2949181a89abd4c8e7020b3c237
MatheusdeOliveiraCorrea <matheus82pessoal@protonmail.com>
2022-08-16 14:11:13 -0300

    step14 custom css

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,49 +1,26 @@
-sap.ui.define
-(
-   [
-      "sap/ui/core/mvc/Controller",
-       "sap/m/MessageToast",
-       "sap/ui/model/json/JSONModel",
-       "sap/ui/model/resource/ResourceModel"
-   ], 
-   function (Controller, MessageToast, JSONModel, ResourceModel) 
-   {
-       "use strict";
-       return Controller.extend
-      ("sap.ui.demo.walkthrough.controller.App", 
-                  {
-                     onInit : function () {
-                        // set data model on view
-                        var oData = {
-                           recipient : {
-                              name : ""
-                           }
-                        };
-                        var oModel = new JSONModel(oData);
-                        this.getView().setModel(oModel);
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
 
-                        // set i18n model on view
-                        var i18nModel = new ResourceModel({
-                        bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-                        });
-
-                        this.getView().setModel(i18nModel, "i18n");
-                     },
-                      onMostrarMensagemAlertaJavaScript : function () 
-                      {
-                         alert("Alert")
-                      },
-
-                      onBotaoDois : function()
-                      {
-                        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                        var nomeDoUsuario = this.getView().getModel().getProperty("/recipient/name");
-
-                        var sMsg = oBundle.getText("helloMsg", [nomeDoUsuario]);
-                        
-                        MessageToast.show(sMsg);
-                      }
-                  }
-      );
-   }
-);
+   "use strict";
+   
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onMostrarMensagemOla : function () 
+      {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         MessageToast.show(sMsg);
+      },
+      onMostrarMensagemOi : function()
+      {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("oiMsg", [sRecipient]);
+         MessageToast.show(sMsg);
+      }
+   });
+});
